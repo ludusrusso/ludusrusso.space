afac6cf25de8d7200a3930bc50988eb045da26a8
Ludovico Russo <l.russo@hoplo.com>
2020-08-28 20:56:58 +0200

    up styles

---
+++ src/components/button.js
@@ -0,0 +1,7 @@
+import React from "react"
+import tw from "tailwind.macro"
+
+export const Button = tw.button`
+  bg-blue-500
+  hover:bg-blue-700
+  text-white font-bold py-2 px-4 rounded`
---
+++ src/components/post-preview.js
@@ -0,0 +1,36 @@
+import React from "react"
+import tw from "tailwind.macro"
+import styled from "@emotion/styled"
+import { Link } from "gatsby"
+
+export const PostPreview = ({ post }) => (
+  <Link
+    href={post.fields.path}
+    css={tw`max-w-sm rounded overflow-hidden shadow hover:shadow-2xl `}
+  >
+    <img
+      css={tw`w-full`}
+      src={post.frontmatter.featureImage.childImageSharp.fixed.src}
+      alt="Sunset in the mountains"
+    />
+    <div css={tw`px-6 py-4`}>
+      <div css={tw`font-bold text-xl mb-2`}>{post.frontmatter.title}</div>
+      <p css={tw`text-gray-700 text-base`}>{post.frontmatter.description}</p>
+    </div>
+    <div class="px-6 pt-4 pb-2"></div>
+  </Link>
+)
+
+const Conainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  gap: 20px;
+`
+
+export const PostPreviewList = ({ posts }) => (
+  <Conainer>
+    {posts.map(post => (
+      <PostPreview post={post} />
+    ))}
+  </Conainer>
+)
